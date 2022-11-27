9843781293233c6dd481a5b938d4af9fbf7ea987
HuyHoang <dragonhuyhoang2001@gmail.com>
2022-11-27 22:20:49 +0700

    Test protected Route

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+
+import NewApp from "./protectedRoutesExp/App2";
+import { BrowserRouter } from "react-router-dom";
+
 import "./index.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-const queryClient = new QueryClient();
+// const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
+    {/* <QueryClientProvider client={queryClient}>
       <App />
       <ReactQueryDevtools initialIsOpen />
-    </QueryClientProvider>
+    </QueryClientProvider> */}
+    <BrowserRouter>
+      <NewApp />
+    </BrowserRouter>
   </React.StrictMode>
 );
